3b35e74a6f8e93474c4ef42e2bc1b61afa0b4a1c
calappidae <2434732764@qq.com>
2020-07-09 10:09:15 +0800

    modifiy ServletUserAdd

--- demo1/src/com/pan/controller/ServletUserAdd.java
+++ demo1/src/com/pan/controller/ServletUserAdd.java
@@ -1,11 +1,21 @@
 package com.pan.controller;
 
 public class ServletUserAdd {
+    private  int age;
+    private  String name;
     public  void run(){
         System.out.println("run......");
     }
 
     public void stop(){
         System.out.println("stop...");
+    }
+
+    @Override
+    public String toString() {
+        return "ServletUserAdd{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
