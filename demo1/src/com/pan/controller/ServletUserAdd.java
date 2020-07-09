2ea496fd4ed0cd85a92d652f36d00cee43db0b3a
calappidae <2434732764@qq.com>
2020-07-09 10:05:42 +0800

    modifiy UserMethon

--- demo1/src/com/pan/controller/ServletUserAdd.java
+++ demo1/src/com/pan/controller/ServletUserAdd.java
@@ -1,4 +1,11 @@
 package com.pan.controller;
 
 public class ServletUserAdd {
+    public  void run(){
+        System.out.println("run......");
+    }
+
+    public void stop(){
+        System.out.println("stop...");
+    }
 }
